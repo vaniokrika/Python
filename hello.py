3f26fbff93229a0610814ce6887983a83462ae60
vaniokrika <44336192+vaniokrika@users.noreply.github.com>
2022-05-10 13:56:32 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+#balgariq na tri moreta
+print("Hello World!")
+"""
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+print("Jam")
+"""
+print("Parenthasis make a lot of code a comment!")
